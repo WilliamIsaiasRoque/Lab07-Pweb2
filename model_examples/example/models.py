fc801d0277b0a1e93533f33d3ee8e90510998f9e
William <wroquequi@unsa.edu.pe>
2023-07-13 17:01:04 -0500

    Agregando modelos Character y Movie para trabajar con muchos a muchos

--- model_examples/example/models.py
+++ model_examples/example/models.py
@@ -25,4 +25,17 @@ class Framework(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name    
+        return self.name
+
+class Movie(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name        
