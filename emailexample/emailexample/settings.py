7749591ff5fd422c65096d47fdc793b9b176e45f
William <wroquequi@unsa.edu.pe>
2023-07-14 04:36:34 -0500

    Agregando configuración de emails a settings.py y modificando views.py

--- emailexample/emailexample/settings.py
+++ emailexample/emailexample/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-PE'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
@@ -117,6 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'willroqlab@gmail.com'
+EMAIL_HOST_PASSWORD = 'bfwx ebfz suge axai'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- emailexample/send/views.py
+++ emailexample/send/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def index(request):
+    send_mail('Hola desde otra cuenta',
+    'Hello there. Esto es un mensaje automático.',
+    'willroqlab@gmail.com',
+    ['wroquequi@unsa.edu.pe'],
+    fail_silently=False)
+
     return render(request, 'send/index.html')
+    
